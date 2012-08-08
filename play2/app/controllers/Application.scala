236a381c387e03ffd9a99a825bf872dddb842a1d
Piyush Purang <ppurang@gmail.com>
2012-08-09 01:24:38 +0200

    introduce play2

---
+++ play2/app/controllers/Application.scala
@@ -0,0 +1,53 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import org.purang.blog.domain._
+import com.sun.xml.internal.ws.resources.SoapMessages
+import com.sun.xml.internal.bind.v2.model.core.NonElement
+
+object Application extends Controller {
+
+  private val database = new collection.mutable.HashMap[String, BlogEntry]()
+
+  def all() = Action {
+    Ok(ListBlogEntryJsonSerializer(database.values.toList)).as("application/json")
+    //Ok("tada")
+  }
+
+  def blog(id: String) = Action {
+    Ok(BlogEntryJsonDeserializer.unapply(database(id))).as("application/json")
+  }
+
+  def createBlog = Action(parse.tolerantText) {
+    request => {
+      import org.purang.blog.domain.NascentBlogEntryJsonDeserializer
+      val entry = convert(NascentBlogEntryJsonDeserializer(request.body))
+      database(entry.uid) = entry
+      Results.Created(entry.uid).withHeaders(
+        LOCATION -> entry.uid
+      )
+    }
+  }
+
+/*
+  def addComment(id: String, ids: String)  = Action(parse.tolerantText) {
+      request => {
+        database.get(id) match {
+          case Some(be) => {
+            //find the comment and update it
+            //not found return bad request
+
+          }
+          case None => NotFound(id)
+        }
+
+        Results.Created(entry.uid).withHeaders(
+          LOCATION -> entry.uid
+        )
+      }
+    }
+*/
+
+
+}
