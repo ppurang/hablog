299590661694f34f366f813acea6f566c245cb39
Piyush Purang <ppurang@gmail.com>
2012-08-28 10:22:53 +0200

    edit form for creating a very simple blog entry + basic authentication + index in elastic search + possibility of turning of example json documents

--- play2/app/controllers/Application.scala
+++ play2/app/controllers/Application.scala
@@ -1,23 +1,110 @@
 package controllers
 
 import play.api._
-import play.api.mvc._
+import libs.ws.WS
+import mvc._
 import org.purang.blog.domain._
 import java.util.UUID.{randomUUID => uuid}
 import org.purang.blog.domain.{Created => c}
 import org.purang.blog.domain.addCommentToList
+import org.purang.net.http.{GET, ApplicationJson, ContentType, POST}
+import scala.Left
+import org.purang.blog.domain.Headline
+import scala.Some
+import scala.Right
+import org.purang.blog.domain.Created
+import org.purang.blog.domain.Section
+import org.purang.blog.domain.User
+import org.purang.blog.domain.Comment
+import org.purang.blog.domain.Text
+import org.purang.blog.domain.Tag
+import org.purang.blog.domain.BlogEntry
+import Utils._
+import net.liftweb.json.NoTypeHints
 
 object Application extends Controller {
+  import org.purang.net.http.ning._
 
+  import play.api.Play.current
   private val database = new collection.mutable.HashMap[String, BlogEntry]()
-  import  Examples._
-  database(e01.uid) = e01
-  database(e02.uid) = e02
-  database(e03.uid) = e03
-  database(e04.uid) = e04
-  database(e05.uid) = e05
+
+  val withExamples: Option[Boolean] = Play.configuration.getBoolean("with.examples")
+  if(withExamples.getOrElse(true)) {
+    import  Examples._
+    database(e01.uid) = e01
+    database(e02.uid) = e02
+    database(e03.uid) = e03
+    database(e04.uid) = e04
+    database(e05.uid) = e05
+  }
+
+  val es: Option[String] = Play.configuration.getString("es.url")
+  val adminSecret = Play.configuration.getString("admin.secret")
+
+  import play.api.data._
+  import play.api.data.Forms._
+  import play.api.data.validation.Constraints._
+
+  case class SubmittedBE(title: Option[String], headline: String, summary: Option[String], text: String) {
+    def asNBE = NascentBlogEntry(title.map(Headline(_)), Headline(headline), summary.map(Text(_)), List(Section(None, Text(text))))
+  }
+
+  val editForm : Form[SubmittedBE]= Form(
+    mapping(
+      "title" -> optional(text),
+      "headline" -> nonEmptyText,
+      "summary" -> optional(text),
+      "section" -> nonEmptyText
+    )(SubmittedBE.apply)(SubmittedBE.unapply)
+  )
+
+  def editView() = Secured("admin", adminSecret.getOrElse("my very secure password")) {
+    Action {
+      Ok(views.html.edit(editForm))
+    }
+  }
+
+  def handleEdit() = Secured("admin", adminSecret.getOrElse("my very secure password")) {
+    Action {
+      implicit request =>
+        Async {
+          editForm.bindFromRequest.fold(
+          errors => play.api.libs.concurrent.Akka.future(BadRequest), {
+            case (e: SubmittedBE) =>
+              val entry: NascentBlogEntry = e.asNBE
+              import org.purang.blog.domain.NascentBlogEntryJsonSerializer
+              WS.url("http://localhost:9000/blog").post(NascentBlogEntryJsonSerializer(entry)).map(
+                response => response.status match {
+                  case 201 => Redirect(routes.Application.index())/*Ok(response.body).withHeaders(
+                      ("Location", response.header("Location").getOrElse("<NO LOCATION HEADER FOUND?>"))
+                    )*/
+                  case x => Results.Status(x).apply(response.body)
+                }
+              )
+          }
+          )
+        }
+    }
+  }
+
   def all() = Action {
-    Ok(ListBlogEntryJsonSerializer(database.values.toList)).as("application/json")
+    implicit val formats = net.liftweb.json.Serialization.formats(NoTypeHints)
+    es.fold(
+      u => (GET > u + "_search?q=title.content:*&sort=created.time:desc&pretty" >> ContentType(ApplicationJson)) ~> {
+        _.fold(
+          t => Results.InternalServerError.apply(t._1.toString),
+          _ match {
+            case (200, _, Some(body), _) => Results.Ok(
+              net.liftweb.json.Serialization.write(
+                ((net.liftweb.json.parse(body) \ "hits") \ "hits" \ "_source")
+            ))
+            case (200, _, None, _) => Results.InternalServerError.apply("200 without a body...")
+            case (status, _, body, _) => Results.Status(status).apply(body.getOrElse("No entity body found"))
+          }
+        )
+      },
+      Ok(ListBlogEntryJsonSerializer(database.values.toList)).as("application/json")
+    )
   }
 
   def index = Action{
@@ -31,12 +118,25 @@ object Application extends Controller {
   def createBlog = Action(parse.tolerantText) {
     request => {
       import org.purang.blog.domain.NascentBlogEntryJsonDeserializer
-      Logger.info(request.body)
+      //Logger.info(request.body)
       val entry = convert(NascentBlogEntryJsonDeserializer(request.body))
       database(entry.uid) = entry
-      Results.Created(entry.uid).withHeaders(
-        LOCATION -> entry.uid
-      )
+      val ixResult = es.map {
+        u => (POST > u + entry.uid >> ContentType(ApplicationJson) >>> BlogEntryJsonSerializer(entry)) ~> {
+          _.fold(
+           t => Results.InternalServerError.apply(t._1.toString),
+          _  match {
+            case (200, _, _, _) => Results.Ok
+            case (status, _, body, _) => Results.Status(status).apply(body.getOrElse("No entity body found"))
+          }
+          )
+        }
+      }
+
+      ixResult.getOrElse(Results.Ok) match {
+        case Results.Ok => Results.Created(entry.uid).withHeaders(LOCATION -> entry.uid)
+        case y => y
+      }
     }
   }
 
@@ -59,10 +159,23 @@ object Application extends Controller {
       }
     }
   }
+
+  //https://gist.github.com/2328236
+  def Secured[A](username: String, password: String)(action: Action[A]) = Action(action.parser) { request =>
+    request.headers.get("Authorization").flatMap { authorization =>
+      authorization.split(" ").drop(1).headOption.filter { encoded =>
+        new String(org.apache.commons.codec.binary.Base64.decodeBase64(encoded.getBytes)).split(":").toList match {
+          case u :: p :: Nil if u == username && password == p => true
+          case _ => false
+        }
+      }.map(_ => action(request))
+    }.getOrElse {
+      Unauthorized.withHeaders("WWW-Authenticate" -> """Basic realm="Secured"""")
+    }
+  }
 }
 
 object Examples {
-
   val e01 = BlogEntry(
       HintedUUIDUniqueIdGenerator("First *blog post* ever"),
       Nascent,
