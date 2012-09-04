9c761c70107206ce389fc5239b6fdd26623b103c
Jose Salguero <jasalguero@gmail.com>
2012-09-05 01:11:37 +0200

    Removed Google loading of jquery library

--- play2/public/js/app.js
+++ play2/public/js/app.js
@@ -1,14 +1,9 @@
+
+
 /******************************************************/
 /*				INITS			            		  */
 /******************************************************/
 var HaBlog = Em.Application.create({
-    ready:function () {
-        this._super();
-
-        HaBlog.InitializeMarkdownParser();
-
-        HaBlog.GetItemsFromServer();
-    }
 });
 
 
@@ -17,7 +12,7 @@ var HaBlog = Em.Application.create({
 /******************************************************/
 
 HaBlog.CONSTANTS = {
-    PATH_CONTEXT : 'http://tech.piyush.purang.net',
+    PATH_CONTEXT : 'http://localhost:9000',
     PATH_BLOG_LIST : '/blog'
 };
 
@@ -27,33 +22,50 @@ HaBlog.Utilities = {
 
 
 HaBlog.GetItemsFromServer = function () {
-    $.getJSON(HaBlog.CONSTANTS.PATH_CONTEXT+HaBlog.CONSTANTS.PATH_BLOG_LIST,
-        function(data) {
+    $.ajax({
+        url:HaBlog.CONSTANTS.PATH_CONTEXT+HaBlog.CONSTANTS.PATH_BLOG_LIST,
+        async:false,
+        success: function(data) {
             // Use map to iterate through the items and create a new JSON object for
             // each item
             data.map(function(item) {
                 //console.log(item);
 
-                var post = {};
+                var post = HaBlog.Post.create();
 
-                post.uid = item.uid;
-                post.headline =  HaBlog.Utilities.Showdown.makeHtml(item.headline.content);
-                post.title =  HaBlog.Utilities.Showdown.makeHtml(item.title.content);
-                post.author = item.author;
-                post.summary = HaBlog.Utilities.Showdown.makeHtml(item.summary.content);
-                post.sections = HaBlog.ParseSections(item.content);
-                post.tags = item.tags;
-                post.comments = HaBlog.ParseComments(item.comments);
-                post.created = moment(item.created.time);
+                post.set('uid', item.uid);
+                post.set('headline', HaBlog.Utilities.Showdown.makeHtml(item.headline.content));
+                post.set('title', HaBlog.Utilities.Showdown.makeHtml(item.title.content));
+                post.set('author', item.author);
+                post.set('summary', HaBlog.Utilities.Showdown.makeHtml(item.summary.content));
+                post.set('sections', HaBlog.ParseSections(item.content));
+                post.set('tags', item.tags);
+                post.set('comments', HaBlog.ParseComments(item.comments));
+                post.set('created', moment(item.created.time));
 
-
-                console.log("Adding new post");
-                //console.log(post);
-                var emberPost = HaBlog.Post.create(post);
-                HaBlog.postsController.addPost(emberPost);
-
+                HaBlog.postListController.addPost(post);
             });
+            console.log("Finished loading");
+        }
     });
+}
+
+HaBlog.CreatePostFromJSon = function (item){
+    //console.log(item);
+
+    var post = HaBlog.Post.create();
+
+    post.set('uid', item.uid);
+    post.set('headline', HaBlog.Utilities.Showdown.makeHtml(item.headline.content));
+    post.set('title', HaBlog.Utilities.Showdown.makeHtml(item.title.content));
+    post.set('author', item.author);
+    post.set('summary', HaBlog.Utilities.Showdown.makeHtml(item.summary.content));
+    post.set('sections', HaBlog.ParseSections(item.content.sections));
+    post.set('tags', item.tags);
+    post.set('comments', HaBlog.ParseComments(item.comments));
+    post.set('created', moment(item.created.time));
+
+    return post;
 }
 
 
@@ -65,13 +77,11 @@ HaBlog.ParseSections = function(jsonContent){
     var sections = [];
 
     jsonContent.map(function(item){
-        //console.log(HaBlog.Utilities.Showdown.makeHtml(item.headline.content));
         var section = HaBlog.Section.create({
             text: item.text === undefined ? null : item.text.content === undefined ? null : HaBlog.Utilities.Showdown.makeHtml(item.text.content),
             headline : item.headline === undefined ? null : item.headline.content === undefined ? null : HaBlog.Utilities.Showdown.makeHtml(item.headline.content)
 
         });
-        console.log(section.text)
         sections.push(section);
     });
     return sections;
@@ -88,7 +98,7 @@ HaBlog.ParseComments = function (jsonContent){
                 likes: item.rating === undefined ? 0 : item.rating.likes === undefined ? 0 : item.rating.likes,
                 dislikes: item.rating === undefined ? 0 : item.rating.dislikes === undefined ? 0 : item.rating.dislikes
             }),
-            replies: item.replies
+            replies: HaBlog.ParseComments(item.replies)
         });
         comments.push(comment);
     });
@@ -106,12 +116,24 @@ HaBlog.Post = Em.Object.extend({
     headline:null,
     state:null,
     author:null,
-    created:moment().subtract('years', 100),
+    created:null,
+    creationDay: function (){
+        return moment(this.get('created')).format('DD');
+    }.property('created'),
+    creationMonth: function (){
+        return moment(this.get('created')).format('MMM');
+    }.property('created'),
+    creationYear:function (){
+        return moment(this.get('created')).format('YYYY');
+    }.property('created'),
     summary:null,
     sections:null,
     tags:null,
     rating:null,
-    comments:null
+    comments:[],
+    commentsCount: function() {
+        return(this.get('comment') == undefined ? 0 : this.get('comments').get('length'));
+    }.property('comments')
 });
 
 // SECTION ITEM
@@ -124,9 +146,12 @@ HaBlog.Section = Em.Object.extend({
 HaBlog.Comment = Em.Object.extend({
     user:null,
     text:null,
-    created:moment().subtract('years', 100),
+    created: moment().subtract('years', 100),
+    createdAgo: function(){
+        return (this.get('created').fromNow());
+    }.property('created'),
     rating:null,
-    replies:null
+    replies:[]
 });
 
 // RATING COMPONENT
@@ -139,19 +164,26 @@ HaBlog.Rating = Em.Object.extend({
 /*				CONTROLLERS							  */
 /******************************************************/
 
+// Define the main application controller. This is automatically picked up by
+// the application and initialized.
+HaBlog.ApplicationController = Ember.Controller.extend({
+
+});
+
 // POST LIST CONTROLLER
-HaBlog.postsController = Em.ArrayController.create({
+HaBlog.PostListController = Ember.ArrayController.extend({
     //container with the list of posts
     content:[],
 
     //add a new post to the list checking that it was not previously there and ordered by creationg date
     addPost:function (post) {
+        //console.log(post);
         // Check to see if there are any post in the controller with the same uid already
         var exists = this.filterProperty('uid', post.uid).length;
         if (exists === 0) {
             // If no results are returned, we insert the new item into the data controller in order of publication date
             var length = this.get('length'), idx;
-            idx = this.binarySearch(Date.parse(post.get('created')), 0, length);
+            idx = this.binarySearch(Date.parse(post.created), 0, length);
             this.insertAt(idx, post);
             return true;
         }
@@ -164,7 +196,7 @@ HaBlog.postsController = Em.ArrayController.create({
             return low;
         }
         mid = low + Math.floor((high - low) / 2);
-        midValue = Date.parse(this.objectAt(mid).get('created'));
+        midValue = Date.parse(this.objectAt(mid).created);
 
         if (value < midValue) {
             return this.binarySearch(value, mid + 1, high);
@@ -172,9 +204,15 @@ HaBlog.postsController = Em.ArrayController.create({
             return this.binarySearch(value, low, mid);
         }
         return mid;
-    },
-    commentsCount : function() {
-        return this.get('comments').get('length');
+    }
+});
+
+// Define the main application controller. This is automatically picked up by
+// the application and initialized.
+HaBlog.PostController = Ember.ObjectController.extend({
+    postedAgo: function(){
+        console.log("here");
+        return "hey";
     }
 });
 
@@ -182,30 +220,57 @@ HaBlog.postsController = Em.ArrayController.create({
 /*				VIEWS								  */
 /******************************************************/
 // View for the Post list
-HaBlog.PostSummaryListView = Em.View.extend({
-    tagName:'article',
-    template:'post-list-view',
-    // Returns the creation date
-    creationDay:function () {
-        var d = this.get('content').get('created');
-        return moment(d).format('DD');
-    }.property('HaBlog.postsController.@each.created'),
-    creationMonth:function () {
-        var d = this.get('content').get('created');
-        return moment(d).format('MMM');
-    }.property('HaBlog.postsController.@each.created'),
-    creationYear:function () {
-        var d = this.get('content').get('created');
-        return moment(d).format('YYYY');
-    }.property('HaBlog.postsController.@each.created'),
-    toggleButtons:function (event, view) {
-        this.$('.fullText').fadeToggle("400", "linear", function () {
-            $(this).closest(".post").find(".toggleButton").toggle();
-        });
-    },
-    // A 'property' that returns the count of items
-    commentsCount: function() {
-        return(this.get('content').get('comments').get('length'));
-    }.property('HaBlog.postsController.@each.comments')
+HaBlog.PostListView = Em.View.extend({
+    templateName:'postList'
 });
 
+//View for the single Post
+HaBlog.PostView = Em.View.extend({
+    templateName:'post'
+});
+
+HaBlog.ApplicationView = Ember.View.extend({
+    templateName: 'application'
+});
+
+
+/******************************************************/
+/*				ROUTER								  */
+/******************************************************/
+
+HaBlog.Router = Ember.Router.extend({
+    root: Ember.Route.extend({
+        index: Ember.Route.extend({
+            route: '/',
+            redirectsTo: 'posts'
+        }),
+        showMain: function(router, context) {
+            console.log("routing to main page");
+            router.transitionTo('posts');
+        },
+        posts: Ember.Route.extend({
+            route: '/posts',
+            showPost: Ember.Route.transitionTo('post'),
+            connectOutlets: function(router) {
+                console.log("routing to post list");
+                router.get('applicationController').connectOutlet('postList');
+            }
+        }),
+        post: Ember.Route.extend({
+            route: '/posts/:uid',
+            connectOutlets: function(router, post) {
+                console.log("redirecting to post")
+                var targetPost = HaBlog.postListController.findProperty('uid', post.uid);
+                router.get('applicationController').connectOutlet('post', targetPost);
+            }
+        })
+    })
+});
+
+
+$(function() {
+    HaBlog.InitializeMarkdownParser();
+    HaBlog.postListController = HaBlog.PostListController.create();
+    //HaBlog.GetItemsFromServer();
+    HaBlog.initialize();
+});
--- play2/public/js/dev-helper.js
+++ play2/public/js/dev-helper.js
@@ -4,29 +4,137 @@
 
 function addSinglePost(){
     testPost = HaBlog.Post.create({
-        uid:"1",
-        title:"Test Title",
-        headline:"headline",
-        tags:[
-            {
-                tag:'blog'
-            },
-            {
-                tag:'write'
-            }
-        ],
-        author:'pprang',
-        sections:[
-            {
-                text:'It would have been so easy -- just pick a blogging platform and\n voila one has a blog with a lot of functionality. In case you really want to own it just roll out one of the many blogging suites on your server. But **none** can boast of the pain and joy creating your own software\n        brings with it. The creativity, the decision making, the struggles to implement the vision at the micro and macro level,\n        and other nitty gritties spice up the adventure.'
-            },
-            {
-                headline:'The need',
-                text:"<p class =\"bla\">[Twitter](http://twitter.com) is too terse;\n most journals have more ads then real content;\n a private diary isn't social enough.</p><p>A good blog is about content, content and content.\n A great or unique writing style makes the difference when content is not a problem.\n        All other things equal a great design and technological schnickschnack give the blog that aura of invincibility.</p>"
-            }
-        ],
-        created:moment()
+        uid:"First-blog-post-ever_aa826639-d829-48b1-b394-8f4ef0d41016",
+        state:"Nascent",
+        created:{
+            time:1343224588265
+        },
+        title:{
+            content:"Finally, First *blog post* ever"
+        },
+        headline:{
+            "content":"First *blog post* ever"
+        },
+        summary:{
+            "content":"This journey took a long time. It almost never started and then it had many close calls. This post narrates this story."
+        },
+        "content":{
+            "sections":[{
+                "text":{
+                    "content":"It would have been so easy -- just pick a blogging platform and  voila one has a blog with a lot of functionality. In case you really want to own it just roll out one of the many blogging suites on your server. But **none** can boast of the pain and joy creating your own software  brings with it. The creativity, the decision making, the struggles to implement the vision at the micro and macro level,  and other nitty gritties spice up the adventure."
+                }
+            },{
+                "headline":{
+                    "content":"The need"
+                },
+                "text":{
+                    "content":"<p class=\"bla\">[Twitter](http://twitter.com) is too terse;  most journals have more ads then real content;  a private diary isn't social enough.</p><p>A good blog is about content, content and content.  A great or unique writing style makes the difference when content is not a problem.  All other things equal a great design and technological schnickschnack give the blog that aura of invincibility.</p>"
+                }
+            }]
+        }
     });
 
-    HaBlog.postsController.addPost(testPost);
+    HaBlog.postListController.addPost(testPost);
+}
+
+function addSingleJsonPost(){
+
+    var item = {
+        "uid":"First-blog-post-ever_aa826639-d829-48b1-b394-8f4ef0d41016",
+        "state":"Nascent",
+        "created":{
+            "time":1343224588265
+        },
+        "title":{
+            "content":"Finally, First *blog post* ever"
+        },
+        "headline":{
+            "content":"First *blog post* ever"
+        },
+        "summary":{
+            "content":"This journey took a long time. It almost never started and then it had many close calls. This post narrates this story."
+        },
+        "content":{
+            "sections":[{
+                "text":{
+                    "content":"It would have been so easy -- just pick a blogging platform and  voila one has a blog with a lot of functionality. In case you really want to own it just roll out one of the many blogging suites on your server. But **none** can boast of the pain and joy creating your own software  brings with it. The creativity, the decision making, the struggles to implement the vision at the micro and macro level,  and other nitty gritties spice up the adventure."
+                }
+            },{
+                "headline":{
+                    "content":"The need"
+                },
+                "text":{
+                    "content":"<p class=\"bla\">[Twitter](http://twitter.com) is too terse;  most journals have more ads then real content;  a private diary isn't social enough.</p><p>A good blog is about content, content and content.  A great or unique writing style makes the difference when content is not a problem.  All other things equal a great design and technological schnickschnack give the blog that aura of invincibility.</p>"
+                }
+            }]
+        },
+        "tags":[{
+            "tag":"blog"
+        },{
+            "tag":"writing"
+        }],
+        "rating":{
+            "likes":1,
+            "dislikes":0
+        },
+        "comments":[{
+            "uid": "123",
+            "user":{
+                "twitterId":"@agreeable"
+            },
+            "text":"totally agree",
+            "created":{
+                "time":1343224588265
+            },
+            "rating":{
+                "likes":1,
+                "dislikes":0
+            },
+            "replies":[{
+                "uid":"7890",
+                "user":{
+                    "twitterId":"@disagreeable"
+                },
+                "text":"totally agree with this commenter",
+                "created":{
+                    "time":1343224588265
+                },
+                "rating":{
+                    "likes":0,
+                    "dislikes":1
+                },
+                "replies":[]
+            }]
+        },{
+            "uid":"1234",
+            "user":{
+                "twitterId":"@disagreeable"
+            },
+            "text":"totally disagree",
+            "created":{
+                "time":1343224588265
+            },
+            "rating":{
+                "likes":0,
+                "dislikes":1
+            },
+            "replies":[{
+                "uid":"5678",
+                "user":{
+                    "twitterId":"@agreeable"
+                },
+                "text":"totally disagree with this commenter",
+                "created":{
+                    "time":1343224588265
+                },
+                "rating":{
+                    "likes":0,
+                    "dislikes":1
+                },
+                "replies":[]
+            }]
+        }]
+    }
+    var post = HaBlog.CreatePostFromJSon(item);
+    HaBlog.postListController.addPost(post);
 }
